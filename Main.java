0ea38a5c90016e093bea70a67f97db6672ecd634
henriquesantosgit <164537714+henriquesantosgit@users.noreply.github.com>
2024-09-13 18:44:49 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,32 @@
 package br.edu.fatecpg.produto.view;
 
+import java.util.Scanner;
 import br.edu.fatecpg.produto.model.Produto;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Criação de um objeto Produto
-        Produto produto1 = new Produto("Caneta", 2.50, 100);
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        System.out.println("Digite o nome do produto:");
+        String nome = scanner.nextLine();
 
-        // Exibindo os detalhes do produto
-        System.out.println(produto1);
+       
+        System.out.println("Digite o preço do produto:");
+        double preco = scanner.nextDouble();
 
-        // Tentando definir um preço negativo
-        System.out.println("\nTentando definir preço negativo:");
-        produto1.setPreco(-5.00);
+        
+        System.out.println("Digite a quantidade em estoque:");
+        int quantidadeEstoque = scanner.nextInt();
 
-        // Tentando definir uma quantidade de estoque negativa
-        System.out.println("\nTentando definir estoque negativo:");
-        produto1.setQuantidadeEstoque(-10);
+       
+        Produto produto = new Produto(nome, preco, quantidadeEstoque);
 
-        // Definindo um preço válido
-        System.out.println("\nDefinindo preço válido:");
-        produto1.setPreco(3.00);
-        System.out.println(produto1);
+      
+        System.out.println("\nProduto criado com sucesso:");
+        System.out.println(produto);
 
-        // Definindo uma quantidade de estoque válida
-        System.out.println("\nDefinindo estoque válido:");
-        produto1.setQuantidadeEstoque(200);
-        System.out.println(produto1);
+        scanner.close();  
     }
 }
